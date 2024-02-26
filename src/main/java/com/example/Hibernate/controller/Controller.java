3b71ed20c74d65a23cd50fe4d3d1bd42527d7753
JDrims <dimas.asus@mail.ru>
2024-02-26 23:19:40 +0600

    add code Spring Security

--- src/main/java/com/example/Hibernate/controller/Controller.java
+++ src/main/java/com/example/Hibernate/controller/Controller.java
@@ -3,6 +3,7 @@ package com.example.Hibernate.controller;
 import com.example.Hibernate.entity.Person;
 import com.example.Hibernate.repository.Repository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +22,19 @@ public class Controller {
     }
 
     @GetMapping("/persons/by-age")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public List<Person> findPersonByAgeOrderByAgeAsc(@RequestParam(value = "age", required = false) Integer age) {
         return repository.findPersonByAgeOrderByAgeAsc(age);
     }
 
     @GetMapping("/persons/by-name&surname")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public Optional<Person> findPersonByNameAndSurnameAllContainingIgnoreCase(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname) {
         return repository.findPersonByNameAndSurnameAllContainingIgnoreCase(name, surname);
+    }
+
+    @GetMapping("/persons/welcome")
+    public String messageWelcome() {
+        return "<h1>Welcome</h1>";
     }
 }
